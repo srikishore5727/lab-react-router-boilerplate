f80ddf1cbb407e9e049d5e6b1f19c2a88d04071f
srikishore5727 <srikishore9080676683@gmail.com>
2023-12-16 18:08:18 +0530

    React Routing

---
+++ ReactRouter/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Navbar from './component/Navbar'
+import AllRoutes from './component/AllRoute'
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <AllRoutes/>
+    </div>
+  )
+}
+
+export default App
